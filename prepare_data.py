4bf94802ce33bf4ca11641a8ba5af1a2f3657faa
Che-Ting Lin <allen74100@gmail.com>
2019-04-26 19:39:58 -0400

    update prepare data

--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,23 @@
 import os
 
+def get_body_points(filenames, focus_2ppl="none"):
+    body_points_all_frame = []
+    for filename in filenames:
+        data = json.load(filename)
+        if focus_2ppl == "none":
+            body_points_all_frame.append(data['people'])
+        else:
+            left_ppl, right_ppl = None, None
+            if data['people'][0][0] > data['people'][1][0]:
+                left_ppl = data['people'][0]
+                right_ppl = data['people'][1]
+            else:
+                left_ppl = data['people'][1]
+                right_ppl = data['people'][0]
+            body_points_all_frame.append(left_ppl) if focus_2ppl == "left" else body_points_all_frame.append(right_ppl)
+    return body_points_all_frame
+
+
 data_path = './data/'
 
 lines_video_list = open(data_path + "video_list.csv", "r").readlines()
@@ -9,6 +27,7 @@ features = []
 labels = []
 
 for line_video_list in lines_video_list:  # for each video
+    action_count = 0
     # basic info
     video_name = line_video_list.split(",")[0]
     ppl_focus = line_video_list.split(",")[1]
@@ -30,10 +49,10 @@ for line_video_list in lines_video_list:  # for each video
         frame_end = line_frame_range.rstrip('\n').split(",")[1]
         action_frames = [json for json in all_json_files if frame_start <= int(json.split("_")[-2]) <= frame_end]
         # FOR EACH FRAME, DO SOMETHING
+        action_count +=1
+        lol = get_body_points(action_frames, focus_2ppl=ppl_focus)
+
         #features.append("")  call extract features
         labels.append(video_name[0])
 
-def extract_features(filenames, num_of_ppl=1, focus_2ppl="none"):
-
-
-    return -1
+    print action_count
