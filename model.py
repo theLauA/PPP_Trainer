45eafafab94016267bd2170cd0b1a3f77416e80f
thelaua <jiananliu@umass.edu>
2019-04-29 14:45:28 -0400

    Loso

--- model.py
+++ model.py
@@ -16,20 +16,20 @@ np.random.seed(seed=42)
 data = genfromtxt('features_subject.csv', delimiter=',')
 X, y, S = data[:, :-2], data[:, -2], data[:,-1]
 X[np.isnan(X)] = 0
-X[np.where(X==float("Inf"))] = 0
+X[np.isinf(X)] = 0
 print(X.shape,y.shape,S.shape)
 print(np.unique(S))
 
-
+avgs = []
 for forehand in [1,3,4,5]:
     for backhand in [101,102,103,104,105]:
         for smash in [201,202,203,204]:
-            print(forehand,backhand,smash)
+            #print(forehand,backhand,smash)
             S_mask = np.logical_or(np.logical_or((S==forehand),(S==backhand)),(S==smash))
 
             X_train, X_test, y_train, y_test = X[~S_mask], X[S_mask],y[~S_mask], y[S_mask]
 
-            print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+            #print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
             #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 
@@ -37,7 +37,7 @@ for forehand in [1,3,4,5]:
             #random-forest
             clf = RandomForestClassifier(n_estimators=100, max_depth=3, random_state=0)
             clf = clf.fit(X_train, y_train)
-            y_pred=clf.predict(X_test)
+            #y_pred=clf.predict(X_test)
 
             """
             #decision-tree
@@ -56,7 +56,14 @@ for forehand in [1,3,4,5]:
             y_pred=clf.predict(X_test)
             """
 
-
-            target_names = ['class 0', 'class 1', 'class 2']
-            print(classification_report(y_test, y_pred, target_names=target_names))
-            print accuracy_score(y_test, y_pred)
+            y_pred = clf.predict_proba(X_test)
+            y_pred_mask = np.max(y_pred,axis=1) < 0.4
+            y_pred = np.argmax(y_pred,axis=1)
+            y_pred[y_pred_mask] = 3
+            #print(np.sum(y_pred_mask))
+            #target_names = ['class 0', 'class 1', 'class 2','class 3']
+            #print(classification_report(y_test, y_pred, target_names=target_names))
+            avgs.append(accuracy_score(y_test, y_pred))
+            #print(accuracy_score(y_test, y_pred))
+avgs = np.array(avgs)
+print(np.mean(avgs))
